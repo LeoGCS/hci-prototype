c55dc1806e311a4b7f5ef31f80612f29cc3d499f
kolick123 <svensukarlz@googlemail.com>
2020-12-21 15:29:53 +0100

    javascript

--- js/GruppenFilter.js
+++ js/GruppenFilter.js
@@ -21,19 +21,19 @@ var anzeigen = [
         maxAnzahl: "3",
     }
 ]
-var filterModule = document.getElementById("module") ;
 
-var filterAnzahl = document.getElementById("maxAnzahl") ;
+//var filterModule = document.getElementById("module") ;
 
-var filter = [filterModule, filterAnzahl] ;
+//var filterAnzahl = document.getElementById("maxAnzahl") ;
 
-var result = anzeigen.filter(function (ele) {
-    return ((ele["name"] == document.getElementById(name) || ele["name"] == null) &&
-        ele["module"] == filterModule &&
-        ele["maxAnzahl"] >= filterAnzahl)
-})
 
-console.log(result)
+function filter(filterAnzahl, filterModule ) {
+    var result = anzeigen.filter(function (ele) {
+        return ((ele["name"] == document.getElementById(name) || ele["name"] == null) &&
+            ele["module"] == filterModule &&
+            ele["maxAnzahl"] >= filterAnzahl)
+        console.log(result)}
+}
 
 
 
